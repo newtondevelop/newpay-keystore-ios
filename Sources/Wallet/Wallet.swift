86b30efecf3e0b7292994dfe611d99f73cb8c0aa
Xia Wu <xiawu@newtonproject.org>
2018-07-19 22:10:57 +0800

    change the algorithm secp256k1 to nist256p1

--- Sources/Wallet/Wallet.swift
+++ Sources/Wallet/Wallet.swift
@@ -39,7 +39,7 @@ public class Wallet {
 
     private func getNode(for derivationPath: DerivationPath) -> HDNode {
         var node = HDNode()
-        hdnode_from_seed(seed.bytes, Int32(seed.count), "secp256k1", &node)
+        hdnode_from_seed(seed.bytes, Int32(seed.count), "nist256p1", &node)
         for index in derivationPath.indices {
             hdnode_private_ckd(&node, index.derivationIndex)
         }
